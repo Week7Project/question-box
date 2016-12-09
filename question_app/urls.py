58f228bb342a3404938802eefbecfd203d71b4c8
Maksym Tymtsiv <mtymtsiv@gmail.com>
2016-12-09 14:12:27 -0500

    js update

--- question_app/urls.py
+++ question_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, {'next_page': '/question_app/login/'}, name='logout'),
     url('^', include('django.contrib.auth.urls')),
     url(r'^register/$', question_app.views.register, name='register'),
-    url(r'^profile/$', question_app.views.profile, name='profile')
+    url(r'^profile/$', question_app.views.profile, name='profile'),
+    url(r'^main/$', question_app.views.main, name='main'),
 ]
